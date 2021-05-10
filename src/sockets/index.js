e8d0f60ec8af1741de52c22b85bf2c8014b74278
VictorMaria <victor.abayomi@outlook.com>
2021-05-10 18:22:39 +0100

    testing

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,6 +1,6 @@
 import socketIOClient from 'socket.io-client';
 
 export const connectToSocket = () => {
-    const socket = socketIOClient(process.env.BACKEND_URL);
+    const socket = socketIOClient(process.env.BACKEND_URL, { transports: ['websocket'] });
     return socket;
 }
