c837f20e8d74da70a9f11b1bde9070adac01a76f
VictorMaria <victor.abayomi@outlook.com>
2021-05-10 14:57:52 +0100

    fix

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -3,6 +3,9 @@ import socketIOClient from 'socket.io-client';
 export const connectToSocket = () => {
     const socket = socketIOClient(process.env.BACKEND_URL, {
         withCredentials: true,
+        extraHeaders: {
+            "my-custom-header": "dashwords"
+          }
       });
     return socket;
 }
