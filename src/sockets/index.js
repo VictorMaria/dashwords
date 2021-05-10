49222fe58ea9a2b2d2115883860932835d6f9a8e
VictorMaria <victor.abayomi@outlook.com>
2021-05-10 14:49:07 +0100

    fix

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,7 +1,8 @@
 import socketIOClient from 'socket.io-client';
 
 export const connectToSocket = () => {
-    const socket = socketIOClient(process.env.BACKEND_URL);
+    const socket = socketIOClient(process.env.BACKEND_URL, {
+        withCredentials: true,
+      });
     return socket;
 }
-
